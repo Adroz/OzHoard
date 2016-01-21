4d6bbc082bd09cf2db4907ff993bbf9dcbf7b19e
Adroz <nicholas.k.moores@gmail.com>
2016-01-21 23:00:29 +1100

    Add cursor adapter and loader implementation

---
+++ app/src/main/java/com/workshoporange/android/ozhoard/DealAdapter.java
@@ -0,0 +1,118 @@
+package com.workshoporange.android.ozhoard;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.workshoporange.android.ozhoard.data.DealsContract;
+
+/**
+ * Created by Nik on 21/01/2016.
+ */
+public class DealAdapter extends RecyclerViewCursorAdapter<DealAdapter.ViewHolder> {
+
+    private FragmentManager mSupportFragmentManager;
+    private int mLayout;
+    private boolean mTwoPane;
+
+    public DealAdapter(FragmentManager supportFragmentManager, int layout, Cursor c, boolean twoPane) {
+        super(c);
+        mSupportFragmentManager = supportFragmentManager;
+        mTwoPane = twoPane;
+        mLayout = layout;
+    }
+
+    private String convertCursorRowToUXFormat(Cursor cursor) {
+        // Get row indices for cursor
+        int idx_title = cursor.getColumnIndex(DealsContract.DealEntry.COLUMN_TITLE);
+        int idx_desc = cursor.getColumnIndex(DealsContract.DealEntry.COLUMN_DESC);
+        int idx_date = cursor.getColumnIndex(DealsContract.DealEntry.COLUMN_DATE);
+        int idx_author = cursor.getColumnIndex(DealsContract.DealEntry.COLUMN_AUTHOR);
+        int idx_link = cursor.getColumnIndex(DealsContract.DealEntry.COLUMN_LINK);
+
+//        String highAndLow = formatHighLows(
+//                cursor.getDouble(idx_max_temp),
+//                cursor.getDouble(idx_min_temp));
+
+//        return Utility.formatDate(cursor.getLong(idx_date)) +
+//                " - " + cursor.getString(idx_short_desc) +
+//                " - " + highAndLow;
+        return cursor.getString(idx_title) + " - " +
+                cursor.getString(idx_desc) + "-" +
+                cursor.getString(idx_link) + ", by " +
+                cursor.getString(idx_author);
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(mLayout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final Cursor cursor) {
+//        holder.contentView.setText(convertCursorRowToUXFormat(cursor));
+        holder.contentView.setText("asdlasknda;sdlnas;ldknas;ldknasd;l");
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mTwoPane) {
+                    Bundle arguments = new Bundle();
+                    arguments.putString(DealDetailFragment.ARG_ITEM_ID,
+                            cursor.getString(DealListActivity.COL_DEAL_TITLE));
+                    DealDetailFragment fragment = new DealDetailFragment();
+                    fragment.setArguments(arguments);
+                    mSupportFragmentManager.beginTransaction()
+                            .replace(R.id.deal_detail_container, fragment)
+                            .commit();
+                } else {
+                    Context context = v.getContext();
+                    Intent intent = new Intent(context, DealDetailActivity.class);
+                    intent.putExtra(DealDetailFragment.ARG_ITEM_ID,
+                            cursor.getString(DealListActivity.COL_DEAL_TITLE));
+
+                    context.startActivity(intent);
+                }
+            }
+        });
+    }
+
+    @Override
+    public Cursor swapCursor(Cursor c) {
+        return super.swapCursor(c);
+    }
+
+    /**
+     * Cache of children views for deal item list.
+     */
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView idView;
+        public final TextView contentView;
+//    public final ImageView iconView;
+//    public final TextView dateView;
+//    public final TextView descriptionView;
+//    public final TextView highTempView;
+//    public final TextView lowTempView;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            idView = (TextView) view.findViewById(R.id.id);
+            contentView = (TextView) view.findViewById(R.id.content);
+//        iconView = (ImageView) view.findViewById(R.id.list_item_icon);
+//        dateView = (TextView) view.findViewById(R.id.list_item_date_textview);
+//        descriptionView = (TextView) view.findViewById(R.id.list_item_textview);
+//        highTempView = (TextView) view.findViewById(R.id.list_item_high_textview);
+//        lowTempView = (TextView) view.findViewById(R.id.list_item_low_textview);
+        }
+    }
+}
--- app/src/main/java/com/workshoporange/android/ozhoard/DealListActivity.java
+++ app/src/main/java/com/workshoporange/android/ozhoard/DealListActivity.java
@@ -2,35 +2,27 @@ package com.workshoporange.android.ozhoard;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.workshoporange.android.ozhoard.data.DealsContract;
 import com.workshoporange.android.ozhoard.deal.DealContent;
 
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-import org.xmlpull.v1.XmlPullParserFactory;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.StringReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -41,19 +33,55 @@ import java.util.List;
  * item details. On tablets, the activity presents the list of items and
  * item details side-by-side using two vertical panes.
  */
-public class DealListActivity extends AppCompatActivity {
+public class DealListActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<Cursor> {
 
     /**
-     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
-     * device.
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet device.
      */
     private boolean mTwoPane;
 
-    public SimpleItemRecyclerViewAdapter mDealsAdapter = new SimpleItemRecyclerViewAdapter(DealContent.ITEMS);
+    private DealAdapter mDealsAdapter;
+    private RecyclerView mRecyclerView;
+    private int mPosition = ListView.INVALID_POSITION;
+
+    private static final int DEALS_LOADER = 0;
+    // Only create a projection of the columns we need.
+    private static final String[] DEAL_COLUMNS = {
+            // The id needs to be fully qualified with a table name, since the content provider
+            // joins the category & deal tables in the background (both have an _id column).
+            DealsContract.DealEntry.TABLE_NAME + "." + DealsContract.DealEntry._ID,
+            DealsContract.DealEntry.COLUMN_DATE,
+            DealsContract.DealEntry.COLUMN_TITLE,
+            DealsContract.DealEntry.COLUMN_LINK,
+            DealsContract.DealEntry.COLUMN_DESC,
+            DealsContract.DealEntry.COLUMN_AUTHOR,
+            DealsContract.CategoryEntry.COLUMN_CATEGORY_PATH,
+            DealsContract.CategoryEntry.COLUMN_CATEGORY_TITLE
+    };
+    // These indices are tied to DEAL_COLUMNS.  If DEAL_COLUMNS changes, these
+    // must change.
+    static final int COL_DEAL_ID = 0;
+    static final int COL_DEAL_DATE = 1;
+    static final int COL_DEAL_TITLE = 2;
+    static final int COL_DEAL_LINK = 3;
+    static final int COL_DEAL_DESC = 4;
+    static final int COL_DEAL_AUTHOR = 5;
+    static final int COL_CATEGORY_PATH = 6;
+    static final int COL_CATEGORY_TITLE = 7;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportLoaderManager().initLoader(DEALS_LOADER, null, this);
+        // The CursorAdapter will take data from our cursor and populate the ListView.
+        mDealsAdapter = new DealAdapter(
+                getSupportFragmentManager(),
+                R.id.deal_detail_container,
+                null,
+                mTwoPane);
+
         setContentView(R.layout.activity_deal_list);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -66,14 +94,14 @@ public class DealListActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Running FetchFeedTask", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                FetchFeedTask feedTask = new FetchFeedTask();
-                feedTask.execute("deals");
+                FetchDealsTask dealsTask = new FetchDealsTask(getApplicationContext());
+                dealsTask.execute("deals"); // Default category
             }
         });
 
-        View recyclerView = findViewById(R.id.deal_list);
-        assert recyclerView != null;
-        setupRecyclerView((RecyclerView) recyclerView);
+        mRecyclerView = (RecyclerView) findViewById(R.id.deal_list);
+        assert mRecyclerView != null;
+        setupRecyclerView(mRecyclerView);
 
         if (findViewById(R.id.deal_detail_container) != null) {
             // The detail container view will be present only in the
@@ -86,6 +114,39 @@ public class DealListActivity extends AppCompatActivity {
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
         recyclerView.setAdapter(mDealsAdapter);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+//        String categoryPath = Utility.getPreferredLocation(getActivity());
+        String categoryPath = "deals";  // TODO: Get real path
+
+        // Sort order:  Ascending, by date.
+        String sortOrder = DealsContract.DealEntry.COLUMN_DATE + " ASC";
+        Uri dealForCategoryUri = DealsContract.DealEntry.buildDealCategoryWithStartDate(
+                categoryPath, System.currentTimeMillis());
+
+        return new CursorLoader(getApplicationContext(),
+                dealForCategoryUri,
+                DEAL_COLUMNS,
+                null,
+                null,
+                sortOrder);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mDealsAdapter.swapCursor(data);
+        if (mPosition != ListView.INVALID_POSITION) {
+            // If we don't need to restart the loader, and there's a desired position to restore
+            // to, do so now.
+            mRecyclerView.smoothScrollToPosition(mPosition);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mDealsAdapter.swapCursor(null);
     }
 
     public class SimpleItemRecyclerViewAdapter
@@ -173,176 +234,6 @@ public class DealListActivity extends AppCompatActivity {
             @Override
             public String toString() {
                 return super.toString() + " '" + mContentView.getText() + "'";
-            }
-        }
-    }
-
-    /**
-     * Created by Nik on 22/11/2015.
-     */
-    public class FetchFeedTask extends AsyncTask<String, Void, String[]> {
-
-        private final String LOG_TAG = FetchFeedTask.class.getSimpleName();
-
-        /**
-         * Take the String representing the complete deal list in XML Format and
-         * pull out the data we need to construct the Strings needed for the wireframes.
-         */
-        private String[] getDealsDataFromRss(String feedXmlStr, int numDeals)
-                throws XmlPullParserException, IOException {
-
-            /** We're looking for the <code>"<title>"</code> and <code>"<link>"</code> tags (among
-             * others), which appear inside the <code>"<item>"</code> tag. The main tags wanted also
-             * appear within the RSS feed's <code>"<channel>"</code> tag as well, so use
-             * @link{insideItem} to ensure we're in the right tag.
-             */
-            boolean insideItem = false;
-
-            // These are the names of the XML tags that need to be extracted.
-            final String RSS_ITEM = "item";
-            final String RSS_TITLE = "title";
-            final String RSS_LINK = "link";
-
-            final String OB_DATE = "pubDate";
-            final String OB_AUTHOR = "creator";
-
-            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-            factory.setNamespaceAware(true);
-            XmlPullParser xpp = factory.newPullParser();
-            xpp.setInput(new StringReader(feedXmlStr));
-
-            String[] resultStrs = new String[numDeals];
-            ArrayList<String> headlines = new ArrayList<>();
-            ArrayList<String> links = new ArrayList<>();
-            ArrayList<String> dates = new ArrayList<>();
-            ArrayList<String> authors = new ArrayList<>();
-            int count = 0;
-
-
-            int eventType = xpp.getEventType();
-            while (eventType != XmlPullParser.END_DOCUMENT) {
-                if (eventType == XmlPullParser.START_TAG) {
-                    if (xpp.getName().equalsIgnoreCase(RSS_ITEM)) {
-                        insideItem = true;
-                    } else if (xpp.getName().equalsIgnoreCase(RSS_TITLE)) {
-                        if (insideItem) headlines.add(xpp.nextText());  // Extract the headline
-                    } else if (xpp.getName().equalsIgnoreCase(RSS_LINK)) {
-                        if (insideItem)
-                            links.add(xpp.nextText());                  // Extract the deal's link
-                    } else if (xpp.getName().equalsIgnoreCase(OB_DATE)) {
-                        if (insideItem)
-                            dates.add(xpp.nextText());                  // Extract the posting date
-                    } else if (xpp.getName().equalsIgnoreCase(OB_AUTHOR)) {
-                        if (insideItem) authors.add(xpp.nextText());    // Extract the author
-                    }
-                } else if (eventType == XmlPullParser.END_TAG
-                        && xpp.getName().equalsIgnoreCase(RSS_ITEM)) {
-                    insideItem = false;
-                    resultStrs[count] = headlines.get(count) + " - " + links.get(count)
-                            + " (" + dates.get(count) + ", by: " + authors.get(count) + ")";               // Construct string.
-                    count++;
-                }
-                eventType = xpp.next();
-            }
-            return resultStrs;
-        }
-
-        @Override
-        protected String[] doInBackground(String... params) {
-            // If there's no category, then set to default.  Verify size of params.
-            if (params.length == 0) {
-                Log.v(LOG_TAG, "No category input, using default.");
-                params = new String[]{"deals"};
-            }
-
-            // These two need to be declared outside the try/catch
-            // so that they can be closed in the finally block.
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-
-            // Will contain the raw JSON response as a string.
-            String feedXmlStr = null;
-
-            int numDeals = 10;
-
-            try {
-                // Construct the URL for OzBargain's feed.
-                final String OZBARGAIN_BASE_URL = "https://www.ozbargain.com.au/";
-                final String OZBARGAIN_CATEGORY_URL = "cat";
-
-                Uri builtUri = Uri.parse(OZBARGAIN_BASE_URL).buildUpon()
-                        .appendPath(params[0])
-                        .appendPath("feed")
-                        .build();
-
-                URL url = new URL(builtUri.toString());
-
-                Log.v(LOG_TAG, "Constructed URI " + builtUri.toString());
-
-                // Create the request to OzBargain, and open the connection
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                // Read the input stream into a String
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuilder buffer = new StringBuilder();
-                if (inputStream == null) {
-                    // Nothing to do.
-                    return null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    // Since it's XML, adding a newline isn't necessary (it won't affect parsing)
-                    // But it does make debugging a *lot* easier if you print out the completed
-                    // buffer for debugging.
-                    buffer.append(line).append("\n");
-                }
-
-                if (buffer.length() == 0) {
-                    // Stream was empty.  No point in parsing.
-                    return null;
-                }
-                feedXmlStr = buffer.toString();
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Error ", e);
-                // If the code didn't successfully get the deal data, there's no point in attempting
-                // to parse it.
-                return null;
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        Log.e(LOG_TAG, "Error closing stream", e);
-                    }
-                }
-            }
-
-            try {
-                return getDealsDataFromRss(feedXmlStr, numDeals);
-            } catch (XmlPullParserException | IOException e) {
-                Log.e(LOG_TAG, e.getMessage(), e);
-                e.printStackTrace();
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String[] result) {
-            if (result != null) {
-                mDealsAdapter.clear();
-                int id = 0;
-                for (String dealStr : result) {
-                    mDealsAdapter.add(new DealContent.Deal(Integer.toString(id), dealStr, "Blah", dealStr));
-                    id++;
-                }
             }
         }
     }
